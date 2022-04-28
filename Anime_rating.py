d2e4d3f72ec3347fd877918c22cfbc2f930cb635
SlenderSlayer24 <86080078+SlenderSlayer24@users.noreply.github.com>
2022-04-28 14:56:02 -0600

    Episode_Number completed, Anime rating started

--- Anime_rating.py
+++ Anime_rating.py
@@ -3,7 +3,6 @@ import pandas
 def Main():
     anime = pandas.read_csv("anime.csv")
     anime = No_Hentai(anime)
-    Episode_Counter(anime)
 
 # Remove bad animes that people shouldn't watch.
 def No_Hentai(file):
@@ -26,20 +25,3 @@ def No_Hentai(file):
                 else:
                     pass
     return good_rows
-
-def Episode_Counter(anime):
-    avarage = 0
-    episode_count = 0
-    series_count = 0
-    for row in anime:
-        if row[4] == "Unknown":
-            pass
-        else:
-            episode_count += int(row[4])
-            series_count += 1
-    avarage = episode_count/series_count
-    print(f"The total number of anime series is {series_count}.")
-    print(f"The total number of episodes in every anime is {episode_count}.")
-    print(f"The avarage number of anime episodes for all animes is {round(avarage)}.")
-
-Main()
